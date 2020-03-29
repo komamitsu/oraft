a395c35b1a533c22e3e8c0e1b2c7c31a104353c4
Mitsunori Komatsu <komamitsu@gmail.com>
2020-03-29 20:26:58 +0900

    Add chaos_test/run.sh invokes cluster and verifier

--- chaos_test/docker/run_cluster.sh
+++ chaos_test/docker/run_cluster.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-set -ex
+set -ex -o pipefail
 
 script_dir=$(dirname $0)
+root_dir=$script_dir/../..
 
 pushd $script_dir
 rm -f ./oraft-*/oraft.log
 rm -rf ./oraft-*/state/
 popd
 
-pushd $script_dir/../..
-export DOCKER_CONF_DIR=$script_dir
-docker-compose --project-directory . -f $script_dir/docker-compose.yml build
-docker-compose --project-directory . -f $script_dir/docker-compose.yml up
+pushd $root_dir
+export DOCKER_CONF_DIR=./chaos_test/docker
+echo DOCKER_CONF_DIR: $DOCKER_CONF_DIR
+echo script_dir: $script_dir
+echo current dir: $PWD
+docker-compose --project-directory . -f chaos_test/docker/docker-compose.yml build
+docker-compose --project-directory . -f chaos_test/docker/docker-compose.yml up
 popd
+
+trap "kill $pid_of_docker" EXIT
 
---
+++ chaos_test/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -ex -o pipefail
+
+script_dir=$(dirname $0)
+root_dir=$script_dir/..
+verifier_dir=$root_dir/verifier
+wait_after_launch=40
+
+pushd $script_dir
+./docker/run_cluster.sh &
+pid_of_cluster=$!
+popd
+
+trap "kill $pid_of_cluster" EXIT
+
+sleep $wait_after_launch
+
+$verifier_dir/run.sh
+
