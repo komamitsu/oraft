2fc21d81af7a909c418945b725c8655980b7b655
Mitsunori Komatsu <komamitsu@gmail.com>
2021-12-19 00:05:20 +0900

    Improve teardown

--- chaos_test/run.sh
+++ chaos_test/run.sh
@@ -5,17 +5,18 @@ set -ex -o pipefail
 script_dir=$(dirname $0)
 root_dir=$script_dir/..
 verifier_dir=$root_dir/verifier
-wait_after_launch=30
 
 pushd $script_dir
 ./docker/build_cluster.sh
-./docker/run_cluster.sh &
-pid_of_cluster=$!
+./docker/start_cluster.sh
 popd
 
-trap "kill $pid_of_cluster" EXIT
-
-sleep $wait_after_launch
+stop_cluster () {
+    pushd $script_dir
+    ./docker/stop_cluster.sh
+    popd
+}
+trap stop_cluster EXIT
 
 $verifier_dir/run.sh
 
