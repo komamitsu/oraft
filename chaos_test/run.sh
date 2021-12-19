af0979de18a9d3edef415e6afe159735a6cde03e
Mitsunori Komatsu <komamitsu@gmail.com>
2021-12-20 00:05:41 +0900

    Revert sleep

--- chaos_test/run.sh
+++ chaos_test/run.sh
@@ -5,6 +5,7 @@ set -ex -o pipefail
 script_dir=$(dirname $0)
 root_dir=$script_dir/..
 verifier_dir=$root_dir/verifier
+wait_after_launch=30
 
 pushd $script_dir
 ./docker/build_cluster.sh
@@ -17,6 +18,8 @@ stop_cluster () {
     popd
 }
 trap stop_cluster EXIT
+
+sleep $wait_after_launch
 
 $verifier_dir/run.sh
 
