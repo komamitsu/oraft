66c959049bdccaf2560001d0d20231a5e2869f71
Mitsunori Komatsu <komamitsu@gmail.com>
2022-01-10 22:04:08 +0900

    Revert test parameters

--- smoke_test/run.sh
+++ smoke_test/run.sh
@@ -6,11 +6,16 @@ script_dir=$(dirname $0)
 root_dir=$script_dir/..
 verifier_dir=$root_dir/verifier
 
-wait_after_launch=20
-wait_for_each_op=10
-wait_until_exit=600
-verifier_count=4096
-verifier_wait_ms=250
+# Ideal config...
+# wait_after_launch=20
+# wait_for_each_op=10
+# wait_until_exit=600
+# verifier_count=4096
+# verifier_wait_ms=250
+
+wait_after_launch=10
+wait_for_each_op=5
+wait_until_exit=300
 
 pushd $script_dir
 ./scenario.sh $wait_after_launch $wait_for_each_op $wait_until_exit &
@@ -21,5 +26,7 @@ trap "kill $pid_of_scenario" EXIT
 
 sleep $wait_after_launch
 
-$verifier_dir/run.sh -c $verifier_count -w $verifier_wait_ms
+# Ideal config...
+# $verifier_dir/run.sh -c $verifier_count -w $verifier_wait_ms
+$verifier_dir/run.sh
 
