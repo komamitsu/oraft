c050beca32f61b3a7eda7e1b0cf48f7616f5368f
Mitsunori Komatsu <komamitsu@gmail.com>
2022-01-19 23:20:03 +0900

    Increase the difficulty

--- smoke_test/run.sh
+++ smoke_test/run.sh
@@ -6,16 +6,11 @@ script_dir=$(dirname $0)
 root_dir=$script_dir/..
 verifier_dir=$root_dir/verifier
 
-# Ideal config...
-# wait_after_launch=20
-# wait_for_each_op=10
-# wait_until_exit=600
-# verifier_count=4096
-# verifier_wait_ms=250
-
-wait_after_launch=10
-wait_for_each_op=5
-wait_until_exit=300
+wait_after_launch=20
+wait_for_each_op=10
+wait_until_exit=600
+verifier_count=4096
+verifier_wait_ms=250
 
 pushd $script_dir
 ./scenario.sh $wait_after_launch $wait_for_each_op $wait_until_exit &
@@ -26,7 +21,5 @@ trap "kill $pid_of_scenario" EXIT
 
 sleep $wait_after_launch
 
-# Ideal config...
-# $verifier_dir/run.sh -c $verifier_count -w $verifier_wait_ms
-$verifier_dir/run.sh
+$verifier_dir/run.sh -c $verifier_count -w $verifier_wait_ms
 
