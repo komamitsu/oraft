0db6dcc9f3b00730c6f88318dd2d52cf5253fc49
Mitsunori Komatsu <komamitsu@gmail.com>
2023-04-23 11:06:32 +0900

    Fix some corrupted charcters

--- lib/append_entries_handler.ml
+++ lib/append_entries_handler.ml
@@ -3,16 +3,16 @@ open Cohttp_lwt_unix
 open Yojson.Basic
 open State
 
-(* Invoked by leader to replicate log entries ($B!x(B5.3); also used as
- * heartbeat ($B!x(B5.2).
+(* Invoked by leader to replicate log entries (§5.3); also used as
+ * heartbeat (§5.2).
  *
  * Receiver implementation:
- * 1. Reply false if term < currentTerm ($B!x(B5.1)
- * 2. Reply false if log doesn$B!G(Bt contain an entry at prevLogIndex
- *     whose term matches prevLogTerm ($B!x(B5.3)
+ * 1. Reply false if term < currentTerm (§5.1)
+ * 2. Reply false if log doesn't contain an entry at prevLogIndex
+ *     whose term matches prevLogTerm (§5.3)
  * 3. If an existing entry conflicts with a new one (same index
  *    but different terms), delete the existing entry and all that
- *    follow it ($B!x(B5.3)
+ *    follow it (§5.3)
  * 4. Append any new entries not already in the log
  * 5. If leaderCommit > commitIndex, set commitIndex =
  *    min(leaderCommit, index of last new entry)
